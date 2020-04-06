a229f1d8191eb445ca3354cd26f08ecfc85f72ca
niinab <niina.blom@myy.haaga-helia.fi>
2020-04-06 14:23:29 +0300

    add ja edit toiminnot

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -3,10 +3,13 @@ import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import Addcar from './Addcar';
+import Editcar from './Editcar';
 
 export default function Carlist() {
     const [cars, setCars] = React.useState([]);
     const [open, setOpen] = React.useState(false);
+    const [msg, setMsg] = React.useState('');
 
     React.useEffect(()=> {
         getCars();
@@ -17,17 +20,54 @@ export default function Carlist() {
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err))
-
     }
 
     const deleteCar = (link) => {
        if (window.confirm('Are you sure?')) {
         fetch(link, {method: 'DELETE'})
         .then(response => getCars())
-        .then(_ => setOpen(true))
+        .then(_ => {
+        setMsg('Car deleted');
+        setOpen(true);
+        })
         .catch(err => console.error(err))
     }
 }
+
+    const addCar = (car) => {
+        fetch('https://carstockrest.herokuapp.com/cars',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(car)
+            }
+        )
+        .then(response => getCars())
+        .then(_ => {
+            setMsg('New car added');
+            setOpen(true);
+        })
+        .catch(err => console.error(err))
+    }
+
+    const updateCar = (link, car) => {
+        fetch(link, {
+            method: 'PUT',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify(car)
+        })
+        .then(response => getCars())
+        .then(_ => {
+            setMsg('Car updated');
+            setOpen(true);
+        })
+        .catch(err => console.error(err))
+    }
+
     const handleClose = () => {
     setOpen(false);
 }
@@ -54,28 +94,33 @@ export default function Carlist() {
         accessor: 'fuel'
     },
     {
-        Header: 'Price',
+        Header: 'Price €',
         accessor: 'price'
     },
     {
-        Cell: row => (<Button size="small" color="secondary" onClick={() => deleteCar(row.original._links.self.href)}>Delete</Button>)
+        Cell: row => (<Editcar car={row.original} updateCar={updateCar} />)
+    },
+    {
+        accessor: '_links.self.href',
+        filterable: false,
+        sortable: false,
+        minWidth: 60,
+        Cell: row => (<Button size="small" color="secondary" onClick={() => deleteCar(row.value)}>Delete</Button>)
     }
     ]
 
     return(
     <div>
-        <ReactTable defaultPageSize={10} filterable={true}data={cars} columns={columns} />
+        <Addcar addCar={addCar}/>
+        <ReactTable defaultPageSize={10} filterable={true} data={cars} columns={columns} />
         <Snackbar
-        open={open}
-        autoHideDuration={3000}
-        onClose={handleClose}
-        message='Car deleted'
+        open={open} autoHideDuration={3000} onClose={handleClose}
+        message={msg}
         anchorOrigin={{
             vertical: 'bottom',
             horizontal: 'left'
         }}
     />
     </div>
-
     );
-}
+    }
